722d9a8703c407984705ff43bcf57cdc559ed1c1
Dmitriy <shadowgods@mail.ru>
2022-07-11 22:06:20 +0500

    Задание номер 5, Повышение зарплаты

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        short weight = 7000;
-        short maxLoss = 500;
-        short minLoss = 250;
-        int timemax = weight/minLoss;
-        int timemin = weight/maxLoss;
-        int timeAverage = (timemax + timemin)/2;
-        System.out.println("Дней для потери веса при минимальных потерях " + timemax+ " Дней");
-        System.out.println("Дней для потери веса при максимальных потерях " + timemin+ " Дней");
-        System.out.println("Дней для потери веса в среднем " + timeAverage+ " День");
-         }
+        int Masha = 67_760;
+        int Denis = 83_690;
+        int Kristina = 76_230;
+     double increaseMasha = Masha*0.1 + Masha;
+     double increaseDenis = Denis*0.1 + Denis;
+     double increaseKristina = Kristina*0.1 + Kristina;
+     double yearSalaryMasha = increaseMasha*12 - Masha*12;
+     double yearSalaryDenis = increaseDenis*12 - Denis*12;
+     double yearSalaryKristina = increaseKristina*12 - Kristina*12;
+        System.out.println("Зарплата Маши теперь " + increaseMasha + ", Годовая зарплата Маши выросла на " + yearSalaryMasha );
+        System.out.println("Зарплата Дениса теперь " + increaseDenis+ ", Годовая зарплата Дениса выросла на " + yearSalaryDenis );
+        System.out.println("Зарплата Кристины теперь " + increaseKristina+ ", Годовая зарплата Кристины выросла на " + yearSalaryKristina );
+    }
 }
